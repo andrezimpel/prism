d0384fc185dff335d503c9fd07590badad9a785d
Andre Zimpel <az@yum.de>
2014-09-21 23:13:22 +0200

    rvm

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ set :stage, :production
 
 set :deploy_to, '/var/www/prism'
 
-# set :rvm_custom_path, '/usr/local/rvm/bin'
+set :rvm_custom_path, '/usr/local/rvm/'
 
 # Replace 127.0.0.1 with your server's IP address!
 server '178.62.234.154', user: 'deploy', roles: %w{web app}
