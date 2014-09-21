e08064dc2975b2c307b71ede642a279ec8a661f7
Andre Zimpel <az@yum.de>
2014-09-21 22:05:23 +0200

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,87 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+# # config valid only for Capistrano 3.1
+# lock '3.1.0'
+#
+# set :application, 'prism'
+# set :repo_url, 'git@github.com:andrezimpel/prism.git'
+#
+# # Default branch is :master
+# # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+#
+# # Default deploy_to directory is /var/www/my_app
+# # set :deploy_to, '/var/www/my_app'
+# set :deploy_to, '/var/www/prism'
+#
+# # Default value for :scm is :git
+# # set :scm, :git
+#
+# # Default value for :format is :pretty
+# # set :format, :pretty
+#
+# # Default value for :log_level is :debug
+# # set :log_level, :debug
+#
+# # Default value for :pty is false
+# # set :pty, true
+#
+# # Default value for :linked_files is []
+# #set :linked_files, %w{config/database.yml}
+#
+# # Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+#
+# # Default value for default_env is {}
+# # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+#
+# # Default value for keep_releases is 5
+# # set :keep_releases, 5
+#
+# namespace :deploy do
+#
+#   desc 'Restart application'
+#   task :restart do
+#     on roles(:app), in: :sequence, wait: 5 do
+#       execute :touch, release_path.join('tmp/restart.txt')
+#     end
+#   end
+#
+#   after :publishing, 'deploy:restart'
+#   after :finishing, 'deploy:cleanup'
+# end
+#
+#
+#
 
 set :application, 'prism'
 set :repo_url, 'git@github.com:andrezimpel/prism.git'
 
-# Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# Default deploy_to directory is /var/www/my_app
 # set :deploy_to, '/var/www/my_app'
-set :deploy_to, '/var/www/prism'
-
-# Default value for :scm is :git
 # set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
+set :rvm_ruby_version, "2.0.0-p353#{fetch(:application)}"
+set :rvm_type, :user
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
 
-# Default value for :pty is false
-# set :pty, true
+set :linked_dirs, [
+  "log",
+  "restricted",
+  "public/system",
+  "solr/production",
+  "solr/staging",
+  "tmp/cache",
+  "tmp/pids",
+  "tmp/sockets",
+  "vendor/bundle",
+]
 
-# Default value for :linked_files is []
-#set :linked_files, %w{config/database.yml}
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
 # set :keep_releases, 5
 
 namespace :deploy do
@@ -40,10 +89,25 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
       execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  after :publishing, 'deploy:restart'
+  desc 'stop solr'
+  task :stop do
+    run "cd #{current_path} && #{rake} RAILS_ENV=#{rails_env} sunspot:solr:stop"
+  end
+
+  desc 'start solr'
+  task :start do
+    run "cd #{current_path} && #{rake} RAILS_ENV=#{rails_env} sunspot:solr:start"
+  end
+
+  desc 'reindex solr'
+  task :reindex do
+    run "cd #{current_path} && #{rake} RAILS_ENV=#{rails_env} sunspot:solr:reindex"
+  end
+
   after :finishing, 'deploy:cleanup'
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,6 @@
 set :stage, :production
 
+set :deploy_to, '/var/www/prism'
+
 # Replace 127.0.0.1 with your server's IP address!
 server '178.62.234.154', user: 'deploy', roles: %w{web app}
