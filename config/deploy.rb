f7494d473c20402fa9970d4b48b9a45195e0ae67
Andre Zimpel <az@yum.de>
2014-09-21 22:01:02 +0200

    deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'git@github.com:andrezimpel/prism.git'
 
 # Default deploy_to directory is /var/www/my_app
 # set :deploy_to, '/var/www/my_app'
-set :deploy_to, '/home/deploy/prism'
+set :deploy_to, '/var/www/prism'
 
 # Default value for :scm is :git
 # set :scm, :git
