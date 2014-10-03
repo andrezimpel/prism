941d7147dbe0b8b58f66efef3a06eca1477b2514
Andre Zimpel <az@yum.de>
2014-10-03 11:54:01 +0200

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "shokeyart@googlemail.com",
-    authentication: "plain",
+    domain: "gmail.com",
+    tls: true,
+    authentication: :plain,
     enable_starttls_auto: true,
     user_name: "shokeyart",
     password: "shokey89"
