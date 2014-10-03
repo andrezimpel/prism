3a47ec18503f97204839d872bd6bc0655b230e55
Andre Zimpel <az@yum.de>
2014-10-03 11:00:55 +0200

    testdeployment

--- app/controllers/backend/accounts_controller.rb
+++ app/controllers/backend/accounts_controller.rb
@@ -1,7 +1,6 @@
 class Backend::AccountsController < Backend::BackendController
   before_action :set_account, only: [:show, :edit, :update, :destroy]
 
-
   # GET /accounts
   # GET /accounts.json
   def index
