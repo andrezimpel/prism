f22ed35973e300aa4ceb2f7473a53768f1878548
Andre Zimpel <az@yum.de>
2014-10-03 11:38:01 +0200

    perform_deliveries = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   # devise
   config.action_mailer.default_url_options = { host: 'priism.de' }
+  config.action_mailer.perform_deliveries = true
 end
